2a6f052d2098739e8d22b1f3b77ac1c36ef331e8
teiderrems <raoul.teida@facsciences-uy1.cm>
2025-04-01 12:04:12 +0200

    remove unnecessary script

--- backend/src/v1/users/stripe.service.ts
+++ backend/src/v1/users/stripe.service.ts
@@ -9,31 +9,6 @@ export class StripeService extends Stripe{
         super(configService.get<string>('SECRET_KEY_STRIPE') as string,{
             maxNetworkRetries:3,
             protocol:'http'
-
         });
     }
-
-    createCustomer(email:string){
-        return this.customers.create({email});
-    }
-
-    getCustomers(limit:number){
-        return this.customers.list({created:{gte: Date.now()}, limit: limit}).autoPagingToArray({limit:10000});
-    }
-
-    customerPayment(email:string,amount:number){
-        // return this.paymentIntents.create({
-        //     application_fee_amount:amount,
-        //     automatic_payment_methods:this.paymentMethods.create()
-
-        // })
-    }
-
-    // addPayementMethod(){
-    //     return this.paymentMethods.create({
-    //         allow_redisplay:'always',
-    //         acc
-            
-    //     })
-    // }
 }
